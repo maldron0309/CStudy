0309b081e50e18ae948e3debb0997545c32b934f
maldron <bagjinyeong640@gmail.com>
2023-07-02 12:54:12 +0900

    if 클리어

--- C-Learning/If.c
+++ C-Learning/If.c
@@ -105,7 +105,7 @@ int main() {
 
 	// if-else 
 
-	int n;
+	/*int n;
 
 	printf("아무 숫자 입력 : ");
 	scanf_s("%d", n);
@@ -120,7 +120,87 @@ int main() {
 	}
 	else {
 		printf("그냥 평범한 숫자 %d", n);
+	}*/
+
+	/*float avg;
+	float math, eng, sci, pro;
+
+	printf("수학,영어,과학,코딩 점수 각각 입력 : ");
+	scanf_s("%f %f %f %f", &math, &eng, &sci, &pro);
+
+	avg = math + eng + sci + pro / 4;
+	printf("당신의 평균 점수 : %f\n", avg);
+
+	if (avg >= 90)
+	{
+		printf("당신은 우등생!");
 	}
+	else if (avg >= 50)
+	{
+		printf("조금만 노력좀...");
+	}
+	else {
+		printf("공부 안하셨나요..?");
+	}*/
+
+	// 논리 연산자
+	/*int a;
+	printf("아무 숫자나 입력 : ");
+	scanf_s("%d", &a);
+
+	if (a >= 10 && a < 20)
+		// && 는 논리곱(AND)
+		// & 는 비트 사이의 AND 연산을 할 때 사용하는 것이고,
+		// && 는 조건식 사이의 관계를 표현 할때 쓰인다
+		// & 는 정확한 비트 AND 연산 결과를 제공하고
+		// && 는 그냥 참 혹은 거짓만을 알려준다
+	{
+		printf("%d는 10 이상, 20미만인 수 입니다\n", a);
+	}*/
+
+	// & && 비교
+
+	/*int a = 31, b = 15;
+	printf("a & b = %d\n", a & b);
+	printf("a && b = %d\n", a && b);*/
+
+	// 논리 합 연산자
+	/*int height, weight;
+	printf("키 와 몸무게를 각각 입력 : ");
+	scanf_s("%d %d", &height, &weight);
+
+	if (height >= 190 || weight >= 100)
+		// AND 와 OR의 차이 AND가 두 조건이 모두 참 일 때 참을 반환한다면
+		// OR 은 두 조건이 모두 거짓 일 때 만 거짓을 반환한다
+		// 참 || 거짓 == 참
+	{
+		printf("당신은 거구 입니다\n", height, weight);
+	}*/
+	// 논리 부정
+	int height, weight;
+	printf("키 와 몸무게를 각각 입력 : ");
+	scanf_s("%d %d", &height, &weight);
+
+	if (height >= 190 || weight >= 100)
+	{
+		printf("당신은 거구 입니다\n", height, weight);
+	}
+	if (!(height >= 190 || weight >= 100))
+		// ! 는 NOT 연산자
+		// 참을 거짓으로, 거짓을 참으로 반전 시켜주는 연산자
+		// 0은 1로, 0이 아닌 값은 0으로 변환해준다
+
+		// ! 연산자가 0 이 아닌 값은 0으로 변환 해준다는 특성을 이용해
+		// 어떠한 변수의 값이 0이 아닌지를 판별하기도 한다
+		
+		// if(!p){ // p == 0 과 같은 의미지만 더 짧다
+		// p가 0 일때만 실행된다.
+		// }
+	{
+		printf("당신 거구가 아닙니다\n");
+	}
+
+
 
 	return 0;
 }
