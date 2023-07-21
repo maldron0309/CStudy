e1f6c706a6fd3dc6a0e677e771f567a3f26c8539
maldron <bagjinhyeong640@gmail.com>
2023-07-22 01:23:33 +0900

    배열 기초 끝

--- C-Learning/Array.c
+++ C-Learning/Array.c
@@ -8,7 +8,7 @@ int main()
 	//printf("Array 5번 째 원소 : %d\n");
 
 	/*int arr[10] = { 2,10,30,21,34,23,53,21,9,1 };
-	
+
 	for (int i = 0; i < arr; i++)
 	{
 		printf("배열의 %d 번째 원소 : %d\n", i + 1, arr[i]);
@@ -37,7 +37,7 @@ int main()
 
 	// 성적 합격 불합격
 
-	int arr[10];
+	/*int arr[10];
 	int i, avg = 0;
 
 	for ( i = 0; i < 10; i++)
@@ -57,9 +57,68 @@ int main()
 		printf("학생 %d", i + 1);
 		if (arr[i] >= avg)
 		{
-
+			printf("합격\n");
 		}
-	}
+		else
+		{
+			printf("불합격\n");
+		}
+	}*/
+
+	// 소수 프로그램
+
+	/*int guess = 5;
+	int prime[1000];
+
+	int index = 1;
+	int i;
+	int ok;
+
+	prime[0] = 2;
+	prime[1] = 3;
+
+	for (;;)
+	{
+		ok = 0;
+		for (i = 0; i <index; i++)
+		{
+			if (guess % prime[i] != 0)
+			{
+				ok++;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		if (ok == (index + 1))
+		{
+			index++;
+			prime[index] == guess;
+
+			printf("소수 : %d\n", prime[index]);
+
+			if (index == 999)
+			{
+				break;
+			}
+			guess += 2;
+		}
+	}*/
+
+	// 상수
+
+    // 상수를 정의시킬 값이 없다면 오류가 발생한다
+	/*const int a = 3;
+
+	printf("%d", a);
+
+
+	const int a = 3; 
+	a = a + 3; // 값을 변경하려고 했기 때문에 오류 발생
+
+	printf("%d", a);*/
 
 	return 0;
 }
