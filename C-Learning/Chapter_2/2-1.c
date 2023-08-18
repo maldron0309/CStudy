de7d147fb876931ece843f5cd63510efa22fa954
maldron <bagjinhyeong640@gmail.com>
2023-08-19 00:06:08 +0900

    폴더 정리 테스트

---
+++ C-Learning/Chapter_2/2-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* 10과 20을 더하는 프로그램 */
+
+int main() {
+	
+	10 + 20;
+
+	return 0;
+}
---
+++ C-Learning/Chapter_2/2-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h> // stdio : Standard input output(표준입출력)의 줄임말
+
+/* 문자열을 화면에 출력하는 프로그램 */
+
+int main() {
+
+	printf("Be happy"); // 문자열 "Be happy" 출력
+	printf("My friend"); // 문자열 "My friend" 출력
+
+	return 0;
+
+}
